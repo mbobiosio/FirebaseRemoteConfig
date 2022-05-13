3ceed3b043f16d85c7cb5895497a8a78432013ab
mbobiosio <cazewonder@gmail.com>
2022-05-14 00:23:48 +0100

    added README.md

---
+++ README.md
@@ -0,0 +1,33 @@
+## App First Launch Example
+
+A simple Android application written in Kotlin to showcase setting app first launch complete process. It uses [DataStore](https://developer.android.com/topic/libraries/architecture/datastore) which is a part of Android Jetpack to store data.
+This application also shows how to use Hilt and viewModel in a simple way.
+
+### Details
+- **Operating System** : Android
+- **Programming Language**: [Kotlin](https://kotlinlang.org)
+- **Dependency Injection** : [Hilt](https://dagger.dev/hilt/)
+- **Fragment Management** : [Navigation Component](https://developer.android.com/guide/navigation/navigation-getting-started)
+- **Data Persistence and Cache** : [DataStore](https://developer.android.com/topic/libraries/architecture/datastore)
+- **Threading** : [Coroutines Kotlin Flow](https://developer.android.com/kotlin/flow)
+- **Design** : [Material Design, Dark Mode and Light Mode](https://material.io)
+
+## Getting started
+
+There are a few ways to open this project.
+
+### Android Studio
+
+1. `Android Studio` -> `File` -> `New` -> `From Version control` -> `Git`
+2. Enter `git@github.com:mbobiosio/FirebaseRemoteConfig.git` into URL field an press `Clone` button
+   3, Build the project and run on an android device or emulator
+
+### Command-line + Android Studio
+
+1. Run `git clone git@github.com:mbobiosio/FirebaseRemoteConfig.git` command to clone project
+2. Open `Android Studio` and select `File | Open...` from the menu. Select cloned directory and press `Open` button
+3. Build the project and run on an android device or emulator
+
+### Author
+
+* [Mbuodile Obiosio](https://linktr.ee/mbobiosio)
--- app/src/main/java/com/mbobiosio/firebaseremoteconfig/data/repository/RemoteConfigRepoImpl.kt
+++ app/src/main/java/com/mbobiosio/firebaseremoteconfig/data/repository/RemoteConfigRepoImpl.kt
@@ -1,7 +1,8 @@
 package com.mbobiosio.firebaseremoteconfig.data.repository
 
-import com.google.firebase.remoteconfig.FirebaseRemoteConfig
-import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import com.mbobiosio.firebaseremoteconfig.BuildConfig
 import com.mbobiosio.firebaseremoteconfig.data.model.RemoteConfigs
 import timber.log.Timber
@@ -14,32 +15,50 @@ import javax.inject.Inject
  */
 class RemoteConfigRepoImpl @Inject constructor() : RemoteConfigRepo {
 
-    private val remoteConfig = FirebaseRemoteConfig.getInstance()
+    // Get remote config instance
+    private val remoteConfig = Firebase.remoteConfig
 
     override fun initConfigs() {
-
+        /**
+         * [cacheInterval] defines the interval of fetches per hour.
+         * Use [remoteConfigSettings] to set the minimum fetch interval
+         * */
         var cacheInterval = TimeUnit.HOURS.toSeconds(12)
         if (BuildConfig.DEBUG) cacheInterval = 0
-        val configSettings = FirebaseRemoteConfigSettings.Builder().apply {
+        val configSettings = remoteConfigSettings {
             minimumFetchIntervalInSeconds = cacheInterval
-        }.build()
+        }
         remoteConfig.setConfigSettingsAsync(configSettings)
+        // [END config settings]
 
+        /*
+        * Set the default parameters for Remote Config
+        * Your app will use these default values until there's a change in the firebase console
+        * */
         val defaults = hashMapOf<String, Any>().apply {
             "force_update" to false
             "message" to "Check update"
         }
         remoteConfig.setDefaultsAsync(defaults)
+        // [END default config]
 
-        remoteConfig.fetchAndActivate().addOnCompleteListener {
-            if (it.isSuccessful) {
-                Timber.d("Successful ${it.result}")
-            } else {
-                Timber.d("Failed ${it.result}")
+        /**
+         * Fetch updates from Firebase console
+         * */
+        remoteConfig.fetchAndActivate()
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Timber.d("Successful ${it.result}")
+                } else {
+                    Timber.d("Failed ${it.result}")
+                }
             }
-        }
+        // [End fetch and activate]
     }
 
+    /**
+     * @return [RemoteConfigs] remote values
+     * */
     override fun getConfigs(): RemoteConfigs {
         return RemoteConfigs(
             forceUpdate = remoteConfig.getBoolean("force_update"),
